68039deb6cb9d17fb657025c8aa08500234b482f
Gabriel Russell <gabrielrussell@Gabriels-MacBook-Air.local>
2020-11-24 12:20:37 -0600

    creates and styles search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React from 'react';
+
+const api = {
+	key: '6ef9a57a6186cdb062cac9bdef08165c',
+	base: 'https://api.openweathermap.org/data/2.5/',
+};
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="app">
+			<main>
+				<div className="search-box">
+					<input type="text" className="search-bar" placeholder="Search..." />
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
